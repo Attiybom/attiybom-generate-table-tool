b9e94ba843c8373d99ac51b84d094bbd4abe0ef7
luminous <910667665@qq.com>
2024-03-09 17:20:46 +0800

    version:1.0.7

--- README.md
+++ README.md
@@ -1,16 +1,46 @@
-## 使用方式
+# vxe-grid表格配置项生成工具
+## 1.使用说明
+* 本工具是基于vxe-table生成的vxe-grid的相关配置项
 
-### 全局安装
+### 1.1 配置项
+#### 1.1.1 列配置
+```js
+  // 列配置
+  const columnsConfig = [
+    //  title: 列标题(可国际化)
+    //  field: 列字段
+    //  visible: 是否显示
+    { title: "Column 1", field: "column1", visible: true },
+    { title: "Column 2", field: "column2", visible: true },
+    { title: "Column 3", field: "column3", visible: false }, // 假设这一列默认不显示
+  ];
+```
+#### 1.1.2表格其他配置项
+```js
+
+  const gridOptions = {
+    minHeight: "500px", // 表格最小高度
+    maxHeight: "800px", //  表格最大高度
+    border: true, // 是否显示边框
+    stripe: true, // 是否显示斑马线
+  };
+
+```
+
+
+## 1.2 使用方式
+
+### 1.2.1 全局安装
 ```
 npm install -g attiybom-generate-table-tool
 ```
 
 
-### 生成命令
+### 1.2.2 生成命令
 ```
 attiybom-generate <jsonName> <filePath>
 ```
-#### 示例
+#### 1.2.2.1 示例
 * 以下命令将会在当前目录的src/views/demo路径下生成table.json文件
 * 请注意路径需要用单引号包裹
 ```
--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 #!/usr/bin/env node
+
 const fs = require('fs');
 const path = require('path');
 
+// 生成含有 columnsConfig 和 gridOptions 的 JSON 文件
 const generateJson = (filename, directoryPath) => {
+  // 列配置
+  const columnsConfig = [
+    //  title: 列标题(可国际化)
+    //  field: 列字段
+    //  visible: 是否显示
+    { title: "Column 1", field: "column1", visible: true },
+    { title: "Column 2", field: "column2", visible: true },
+    { title: "Column 3", field: "column3", visible: false }, // 假设这一列默认不显示
+  ];
+
+  // 表格其他配置
+  const gridOptions = {
+    minHeight: "500px", // 表格最小高度
+    maxHeight: "800px", //  表格最大高度
+    border: true, // 是否显示边框
+    stripe: true, // 是否显示斑马线
+  };
+
   const content = {
     table: {
       name: filename,
-      columns: ["Column 1", "Column 2", "Column 3"],
-      rows: [
-        { "Column 1": "Data 1", "Column 2": "Data 2", "Column 3": "Data 3" },
-        { "Column 1": "Data 4", "Column 2": "Data 5", "Column 3": "Data 6" }
-      ]
-    }
+      columnsConfig,
+      gridOptions,
+    },
   };
 
   // 分割路径，并确保使用系统特定的路径分隔符
@@ -24,6 +41,7 @@ const generateJson = (filename, directoryPath) => {
     fs.mkdirSync(dir, { recursive: true });
   }
 
+  // 写入JSON文件
   fs.writeFileSync(fullPath, JSON.stringify(content, null, 2));
   console.log(`JSON file has been generated at ${fullPath}`);
 };
